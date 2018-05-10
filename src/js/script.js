8756a292ded1b7098915f54109a06bd97c9db52f
Cebula Kamil <kontakt@KamilCebula.pl>
2018-05-10 12:15:23 +0200

    Update JavaScript code and RWD

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,64 @@
-const person = {
-    name: "Marcin",
-    age: 33
+const startGameButton = document.getElementById("start-game");
+let playing = false;
+let score;
+let action;
+let timeRemainingCounter;
+const scoreValueSpan = document.getElementsByClassName("scoreValue");
+const timeRemaining = document.getElementById("time");
+const timeRemainingValue = document.getElementById("time-remaining-value");
+const modalGameOver = document.getElementById("modal-game-over");
+
+const show = function(id) {
+    id.style.display = "block";
 }
 
-console.log(person.name);
+const hide = function(id) {
+    id.style.display = "none";
+}
+
+const generateQA = function() {
+
+}
+
+const startPlayGame = function() {
+    if (playing == true) {
+        location.reload();
+    } else {
+        hide(modalGameOver);
+        playing = true;
+        score = 5;
+        scoreValueSpan[0].innerHTML = score;
+        show(timeRemaining);
+        timeRemainingCounter = 60;
+        timeRemainingValue.innerHTML = timeRemainingCounter;
+        startGameButton.innerHTML = "Reset Game";
+        startCountDown();
+        generateQA();
+    }
+}
+
+const modalDisplayNone = function() {
+    hide(modalGameOver);
+}
+
+const stopCountDown = function() {
+    hide(timeRemaining);
+    clearInterval(action);
+    playing = false;
+    scoreValueSpan[1].innerHTML = score;
+    show(modalGameOver);
+    modalGameOver.addEventListener("click", modalDisplayNone);
+    startGameButton.innerHTML = "Start Game";
+}
+
+const startCountDown = function() {
+    action = setInterval(function(){
+        timeRemainingCounter -= 10;
+        timeRemainingValue.innerHTML = timeRemainingCounter;
+        if (timeRemainingCounter == 0) {
+            stopCountDown();
+        }
+    },1000)
+}
+
+startGameButton.addEventListener("click", startPlayGame);
